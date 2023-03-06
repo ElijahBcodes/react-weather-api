455a1793784bed0fee54f5eea79de747060ad975
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-03-06 13:09:55 -0800

    fix indent

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ const App = () => {
       .catch((error) => console.log(error));
   };
   return (
-    <div className=" h-screen grid place-items-center bg-cover" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")` }}>
+    <div
+      className=" h-screen grid place-items-center bg-cover"
+      style={{
+        backgroundImage: `url("https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")`,
+      }}
+    >
       <div className="bg-white w-92 p-4 rounded-md">
         <div className="flex items-center justify-between">
           <input
